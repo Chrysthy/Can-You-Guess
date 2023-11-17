1ed52b2231fcbe077e50e76140d533e34eaae443
Chrystine Martins <chrystine.martins@outlook.com>
2023-11-16 21:06:29 -0300

    add js

---
+++ index.js
@@ -0,0 +1,24 @@
+//var numeroSecreto = 581
+var numeroSecreto = parseInt(Math.random() * 1001)
+
+var chute = prompt("Digite um número entre 1 e 1000.")
+
+
+while(chute != numeroSecreto) {
+
+  var chute = prompt("Digite um número entre 0 e 1000.")
+ 
+  if (chute == numeroSecreto) {
+     alert("Acertou!")
+  }
+
+  else if (chute > numeroSecreto) {
+    alert("Errou... O número secreto é menor.")
+  }
+
+  else if (chute < numeroSecreto) {
+    alert("Errou... O número secreto é maior.")
+  }
+
+}
+ 
